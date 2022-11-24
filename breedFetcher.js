ce2e4529971592a3e9bc814d893b4faa3daf7dad
Mark Bieganek <mark.bieganek@telus.com>
2022-11-24 10:47:11 -0700

    Make request, parse result as JSON, use promises

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,39 @@
 const request = require('request');
+const inspect = require('util').inspect;
+
+const makeRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error) {
+        return resolve(body);
+      }
+      return reject(error);
+    });
+  });
+};
+
+const parseJSON = (stringJSON) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = JSON.parse(stringJSON);
+      return resolve(data);
+    } catch (error) {
+      return reject(error);
+    };
+  });
+}
+
+// const isValidJSON = (stringJSON) => {
+//   try {
+//     JSON.parse(stringJSON);
+//     return true;
+//   } catch {
+//     return false;
+//   }
+// }
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=bengal';
+makeRequest(url)
+  .then((responseBody) => parseJSON(responseBody))
+    .then((parsedJSON) => console.log(parsedJSON))
+  .catch((error) => console.log(error))
+  .finally(() => console.log('🐱🐱 MEOW 🐱🐱 All done!'));
